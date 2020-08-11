ff2cb86a6d5952a115e22cf9e9c9c31e7703fcbc
Ounaide <69464156+Ounaide@users.noreply.github.com>
2020-08-11 13:57:46 +0200

    Add files via upload

---
+++ transformée de fourier.py
@@ -0,0 +1,43 @@
+from matplotlib.pyplot import *
+from numpy import *
+from easygui import enterbox, msgbox
+from scipy.signal import *
+import sys
+
+x=linspace(0,2*pi,1000)
+
+def enter():   
+    try:
+        global y
+        global sy
+        sy=enterbox("Fonction y(x):")
+        print(sy)
+        y=eval(sy)
+    except:
+        if sy!=None:
+            msgbox("Erreur de syntaxe")
+            enter()
+        else:
+            sys.exit()
+    return y,sy
+
+enter()
+ft=fft.fft(y)
+
+f=figure('transformée de fourier')
+ax1=f.add_subplot(211)
+ax1.plot(x,y,label=f'fonction: {sy}')
+ax1.set_xlabel("t(s)")
+ax1.set_ylabel("signal")
+legend()
+
+ax2=f.add_subplot(212)
+ax2.plot(abs(ft),label='transformée de Fourier',color='r')
+ax2.set_xlabel("fréquence(Hz)")
+ax2.set_ylabel("|A|")
+ax2.set_xlim(0,500)
+ft.sort()
+print(ft[-1:-5])
+
+legend()
+show()
